0a8d929d5c797bdb2c08607722fee9764bc6a2e4
KateAlf <katrin.alf@gmail.com>
2016-07-27 10:53:54 +0300

    :package: Set up Gulp and PostCSS plugins

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+
+var postcss = require("gulp-postcss");
+var cssnext = require("postcss-cssnext");
+var precss = require("precss");
+var atImport = require("postcss-import");
+var mqpacker = require("css-mqpacker");
+var cssnano = require("cssnano");
+var postcsscenter = require("postcss-center");
+var stylelint = require("stylelint");
+var stylefmt = require("stylefmt");
+var postcsssorting = require("postcss-sorting");
+
+var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
+
+var copy = require("gulp-contrib-copy");
+var clean = require("gulp-contrib-clean");
+
+var server = require("browser-sync");
+
+gulp.task("style", function () {
+  var processors = [
+    cssnext,
+    precss,
+    atImport,
+    postcsscenter,
+    postcsssorting,
+    mqpacker ({
+        sort: true })
+    /*cssnano*/
+  ];
+
+  return gulp.src("src/*.css")
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("dest"));
+});
+
+gulp.task("images", function () {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+      .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true
+    }))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function () {
+  gulp.src("img/svg/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("img/svg/mini"));
+});
+
+gulp.task("serve", ["style"], function () {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+});
+
+gulp.task("clean", function () {
+  gulp.src('build', {read: false})
+      .pipe(clean());
+  });
+
+gulp.task("copy", function () {
+  gulp.src("*.html").pipe(gulp.dest("build"));
+  gulp.src("dest/*.css").pipe(gulp.dest("build/css"));
+  gulp.src("*.json").pipe(gulp.dest("build"));
+  gulp.src("fonts/**/*.{woff,woff2}").pipe(gulp.dest("build/fonts"));
+  gulp.src("img/**.{png,jpg,gif,svg}").pipe(gulp.dest("build/img"));
+  gulp.src("js/**.js").pipe(gulp.dest("build/js"));
+});
+
+gulp.watch("src/*.css", ["style"]);
+gulp.watch("dest/*.css").on("change", server.reload);
+gulp.watch("*.html").on("change", server.reload);
+gulp.watch("img/*").on("change", server.reload);
+
+gulp.task("build", ["clean", "style", "images", "copy"], function () {
+});
